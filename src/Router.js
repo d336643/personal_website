3f492382e3d08d39a9148a8fc4afe0ccdd470409
Euni <d336643@gmail.com>
2021-03-23 21:53:15 +0800

    add router

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,18 @@
 import React from "react"
 import HomePage from "./HomePage";
 import ChatPage from "./ChatPage";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, HashRouter as Router} from "react-router-dom";
 
 
 
 function Router() {
     return (
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/chat" component={ChatPage}/>
         </Switch>
+      </Router>
     )
   }
   
